eb5ab6b6d99ad9a786fdc68251db8459d88ce46a
Nick Petersen <nick@nickpetersen.co>
2019-04-25 12:26:01 -0500

    added physic controls for player sphere

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
-    void Update ()
+    public float speed;
+
+    private Rigidbody rb;
+
+    void Start () 
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis ("Vertical");
+        rb = GetComponent<Rigidbody>();
     }
+
     void FixedUpdate () 
     {
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
 
+        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce (movement * speed);
     }
 }
